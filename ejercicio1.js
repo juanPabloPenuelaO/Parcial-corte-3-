666c210a4d25d57567d3c904a31ba03df5ce5fe7
juanPabloPeñuelaO <juanppenuelaocampo@gmail.com>
2023-06-16 11:17:29 -0500

    se ha agregado todo

---
+++ ejercicio1.js
@@ -0,0 +1,48 @@
+let videojuegos = [
+
+{
+    nombre: "minecraft",
+  dispositivo: "todos",
+  online: "si",
+  modo: "supervivencia",
+  creación: "2011"
+},
+
+{
+    nombre: "gta",
+  dispositivo: "play",
+  online: "si",
+  modo: "acción",
+  creación: "2013"
+},
+
+{
+    nombre: "fifa",
+  dispositivo: "play",
+  online: "si",
+  modo: "deportes",
+  creación: "1999"
+},
+
+{
+    nombre: "tetris",
+  dispositivo: "todos",
+  online: "no",
+  modo: "arcade",
+  creación: "1984"
+},
+
+{
+    nombre: "pac-man",
+  dispositivo: "pc",
+  online: "no",
+  modo: "arcade",
+  creación: "1980"
+},
+
+
+
+
+]
+
+console.log(videojuegos);
